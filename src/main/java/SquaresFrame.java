e853c2aaebc3eef609155f135452de365ee13907
esty <ester-agishtein@student.touro.edu>
2020-12-05 21:20:09 -0500

    Added single stanza play button with icon

--- src/main/java/SquaresFrame.java
+++ src/main/java/SquaresFrame.java
@@ -8,6 +8,9 @@ public class SquaresFrame extends JFrame {
     private JButton play;
     private JButton clear;
     private JButton stop;
+    private JPanel playButtonsPanel;
+    private JPanel viewAndButtons;
+    private JPanel UIControlPanel;
     private int delay = 200;
     boolean playing = false;
 
@@ -15,21 +18,42 @@ public class SquaresFrame extends JFrame {
         this.squares = view.getSquares();
         this.view = view;
 
-        setSize(690, 300);
+        setSize(1000, 500);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Musical Squares");
-        setLayout(new BorderLayout());
+        setLayout(new GridLayout(2,1));
         play = new JButton("Play");
         clear = new JButton("Clear");
         stop = new JButton("Stop");
+        playButtonsPanel = new JPanel(new GridLayout(1, squares.COL));
+        UIControlPanel = new JPanel(new GridLayout(1, 3));
+        viewAndButtons = new JPanel(new BorderLayout());
+
         stop.addActionListener(ActionEvent -> stopPlaying());
         clear.addActionListener(ActionEvent -> squares.clearSquares());
         play.addActionListener(ActionEvent -> playNotes());
-        add(play, BorderLayout.EAST);
-        add(clear, BorderLayout.WEST);
-        add(stop, BorderLayout.SOUTH);
+
+        for (int j = 0; j < Squares.COL; j++) {
+                JButton playStanzaButton = new JButton();
+                ImageIcon playIcon = new ImageIcon("icons8-circled-play-64.png");
+                playStanzaButton.setIcon(playIcon);
+
+                int finalStanza = j;
+                playStanzaButton.addActionListener(ActionEvent -> squares.playStanza(finalStanza));
+                playButtonsPanel.add(playStanzaButton);
+            }
+
         view.addMouseListener(listener);
-        add(view, BorderLayout.CENTER);
+        viewAndButtons.add(playButtonsPanel, BorderLayout.NORTH);
+        viewAndButtons.add(view, BorderLayout.CENTER);
+        add(viewAndButtons);
+
+
+        UIControlPanel.add(play);
+        UIControlPanel.add(clear);
+        UIControlPanel.add(stop);
+        add(UIControlPanel);
+
     }
 
     private void stopPlaying() {
