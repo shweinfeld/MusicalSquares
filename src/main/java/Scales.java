7507c4e01b65d1e6b2d8a0d8ab74e649ef9e9697
esty <ester-agishtein@student.touro.edu>
2020-12-06 10:50:17 -0500

    Created multiple scale functionality

---
+++ src/main/java/Scales.java
@@ -0,0 +1,23 @@
+
+
+public enum Scales {
+
+
+    CLASSIC(new int[]{1, 13, 25, 37, 49, 61, 73, 85, 97, 109, 121}),
+    PENTATONIC(new int[]{60, 62, 76, 67, 69, 72, 74, 76, 79, 81, 84, 86}),
+    CHROMATIC(new int[]{72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83}),
+    MAJOR(new int[]{60, 62, 64, 65, 67, 69, 71, 72, 74, 76, 77, 79}),
+    HARMONIC_MINOR(new int[]{69, 71, 72, 76, 76, 77, 80, 81, 83, 84, 86, 88});
+
+    private int[] scale;
+
+    private Scales(int[] scale) {
+        this.scale = scale;
+    }
+
+    public int[] getScale() {
+        return this.scale;
+    }
+
+
+}
--- src/main/java/SquaresFrame.java
+++ src/main/java/SquaresFrame.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public class SquaresFrame extends JFrame {
+public class SquaresFrame extends JFrame implements ItemListener {
 
     private Squares squares;
     private SquaresView view;
@@ -11,6 +13,7 @@ public class SquaresFrame extends JFrame {
     private JPanel playButtonsPanel;
     private JPanel viewAndButtons;
     private JPanel UIControlPanel;
+    private JComboBox<Scales> scaleOptions;
     private int delay = 200;
     boolean playing = false;
 
@@ -21,12 +24,16 @@ public class SquaresFrame extends JFrame {
         setSize(1000, 500);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Musical Squares");
-        setLayout(new GridLayout(2,1));
+        setLayout(new GridLayout(2, 1));
         play = new JButton("Play");
         clear = new JButton("Clear");
         stop = new JButton("Stop");
+        scaleOptions = new JComboBox<>(Scales.values());
+        scaleOptions.addItemListener(this);
+
+
         playButtonsPanel = new JPanel(new GridLayout(1, squares.COL));
-        UIControlPanel = new JPanel(new GridLayout(1, 3));
+        UIControlPanel = new JPanel(new GridLayout(1, 4));
         viewAndButtons = new JPanel(new BorderLayout());
 
         stop.addActionListener(ActionEvent -> stopPlaying());
@@ -34,15 +41,7 @@ public class SquaresFrame extends JFrame {
         play.addActionListener(ActionEvent -> playNotes());
 
         //might want to separate out into a separate function
-        for (int j = 0; j < Squares.COL; j++) {
-                JButton playStanzaButton = new JButton();
-                ImageIcon playIcon = new ImageIcon("icons8-circled-play-64.png");
-                playStanzaButton.setIcon(playIcon);
-
-                int finalStanza = j;
-                playStanzaButton.addActionListener(ActionEvent -> squares.playStanza(finalStanza));
-                playButtonsPanel.add(playStanzaButton);
-            }
+        createPlayStanzaButtons();
 
         view.addMouseListener(listener);
         viewAndButtons.add(playButtonsPanel, BorderLayout.NORTH);
@@ -53,8 +52,21 @@ public class SquaresFrame extends JFrame {
         UIControlPanel.add(play);
         UIControlPanel.add(clear);
         UIControlPanel.add(stop);
+        UIControlPanel.add(scaleOptions);
         add(UIControlPanel);
 
+    }
+
+    private void createPlayStanzaButtons() {
+        for (int j = 0; j < Squares.COL; j++) {
+            JButton playStanzaButton = new JButton();
+            ImageIcon playIcon = new ImageIcon("icons8-circled-play-64.png");
+            playStanzaButton.setIcon(playIcon);
+
+            int finalStanza = j;
+            playStanzaButton.addActionListener(ActionEvent -> squares.playStanza(finalStanza));
+            playButtonsPanel.add(playStanzaButton);
+        }
     }
 
     private void stopPlaying() {
@@ -76,5 +88,13 @@ public class SquaresFrame extends JFrame {
             }
         });
         thread.start();
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent event) {
+
+        if (event.getSource() == scaleOptions) {
+            squares.changeScales((Scales) scaleOptions.getSelectedItem());
+        }
     }
 }
